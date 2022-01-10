457f2b103e4f01b26b0761911ae3f75d4c7e19dc
Josh Gretz <jgretz@truefit.io>
2022-01-10 15:32:19 -0500

    add queues to handle timing of connection

--- src/rabbitmq.service.ts
+++ src/rabbitmq.service.ts
@@ -5,10 +5,28 @@ import {RabbitMessageHandler} from './Types';
 import decode from './decode';
 import encode from './encode';
 
+interface Subscription<T> {
+  queue: string;
+  key: string;
+  handler: RabbitMessageHandler<T>;
+}
+
+interface Message<T> {
+  queue: string;
+  key: string;
+  payload: any;
+  responseHandler?: RabbitMessageHandler<T>;
+}
+
 export class RabbitMqService {
   channel: amqp.Channel;
+  subscriptionQueue: Array<Subscription<unknown>>;
+  messageQueue: Array<Message<unknown>>;
 
   constructor() {
+    this.subscriptionQueue = [];
+    this.messageQueue = [];
+
     amqp.connect(process.env.RABBIT_URL, (error, connection) => {
       if (error) {
         Logger.error(`${process.env.RABBIT_URL}: AMQP Connect Error`, error);
@@ -26,11 +44,27 @@ export class RabbitMqService {
         });
 
         this.channel = channel;
+
+        // clear queues
+        this.subscriptionQueue.forEach(({queue, key, handler}) => {
+          this.subscribe(queue, key, handler);
+        });
+
+        this.messageQueue.forEach(({queue, key, payload, responseHandler}) => {
+          this.send(queue, key, payload, responseHandler);
+        });
       });
     });
   }
 
+  // START HERE - the channel hasnt been established by the time someone calls subscribe
+
   subscribe<T>(queue: string, key: string, handler: RabbitMessageHandler<T>) {
+    if (!this.channel) {
+      this.subscriptionQueue.push({queue, key, handler});
+      return;
+    }
+
     this.channel.consume(queue, async (msg) => {
       const message = decode(msg.content);
       if (message.key !== key) {
@@ -47,6 +81,11 @@ export class RabbitMqService {
   }
 
   send<T>(queue: string, key: string, payload: any, responseHandler?: RabbitMessageHandler<T>) {
+    if (!this.channel) {
+      this.messageQueue.push({queue, key, payload, responseHandler});
+      return;
+    }
+
     const replyKey = responseHandler ? `${key}_${uuidv4()}` : undefined;
     const sendMsg = encode(queue, key, payload, replyKey);
 
